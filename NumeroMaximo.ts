6b6d292279a91f307feea5e7c2b9dd98981536ca
JesicaJustel <justeljesica@gmail.com>
2023-04-16 13:55:41 -0300

    NumeroMaximo

--- NumeroMaximo.ts
+++ NumeroMaximo.ts
@@ -1,13 +1,14 @@
 import*as rls from "readline-sync";
-let numeromaximo:number=rls.questionInt("ingrese numero:");
+let numero:number=rls.questionInt("ingrese un numero");
+let numeromaximo:number=rls.questionInt("ingrese numero maximo:");
 let i:number=0;
 while (numeromaximo !== 0); {
     if(i <= numeromaximo) {
-    i=numeromaximo;}
-    i=numeromaximo;}
+    i=numero;}
+    i=numero -1 ;}
     i=numeromaximo;{
    
-    numeromaximo=rls.questionInt("ingrese numero:");
+    numero=rls.questionInt("ingrese numero:");
     numeromaximo=rls.questionInt("el numero maximo es:");
 }
 console.log("el numero maximo es:"+i);
